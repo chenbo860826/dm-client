94c29adedd86a25329bc7e4c970a10b46444163a
chenbo860826 <bo.chen@moblab.com>
2023-07-20 22:29:19 +0800

    [project] initial version of dm-client

--- README.md
+++ README.md
@@ -1,2 +1,133 @@
-# dm-client
-It is the client library for DM(DataMaster) to make batch and upload objects.
+# DM Client
+
+## Overview
+
+The package is used as client sdk to upload data to DM (DataMaster) platform.
+
+## Examples
+
+The following example shows how to construct, initialize, and add data to the batch.
+
+```
+import { DataBatch, FileField } from 'dm-client'
+
+// construct the batch
+let batch = new DataBatch('https://***.dm.com', 
+		1, 
+		'7be433c9-b332-4177-abeb-a636b017510a',
+		'batch202306010800');
+
+// initialize the batch
+await batch.init();
+
+// add objects to the batch
+await batch.add('example', { key: 'speed', value: 100 });
+await batch.add('example', { key: 'temperature', value: 30 });
+
+// add objects with files attached
+await batch.add('file', { file: new FileField('./overall.css'), name: 'overall.css'});
+
+// complete the batch
+await batch.complete();
+```
+
+## Classes
+
+### DataBatch
+
+### constructor()
+
+*Syntax:* 
+
+`constructor(String serverUrl, Integer collectorId, String collectorAppKey, String batchName)`
+
+*Description:*
+
+It create a DataBatch instance with the following arguments:
+
+- serverUrl: The url of DM platform
+- collectorId: The integer id of the collector. Please contact DM administrator to allocate a collector.
+- collectorAppKey: The appKey of the collector.
+- batchName: Name information that client names the batch
+
+#### init()
+
+*Syntax:*
+
+`Promise(void) init()`
+
+*Description:*
+
+Initialize the batch from DM. It will open a new batch for write if success. Otherwise, it will possibly throw the following Http Errors if error happens.
+
+| Status | Error             | Description                                                  |
+| ------ | ----------------- | ------------------------------------------------------------ |
+| 401    | Invalid Collector | collectorId or collectorAppKey is invalid or doesn't match.  |
+| 409    | Batch Conflict    | another batch is ongoing, client shall try again after it finished. |
+
+#### add()
+
+*Syntax:*
+
+`Promise(void) add(String type, String json)`
+
+*Description:*
+
+It shall provide two arguments:
+
+- type: type label of the data. (The same type of data within collector shall have the same schema)
+- json: the data body in json format.
+
+It will add a json object with the type to the batch if success. Otherwise, it will throw the following Http errors when error happens.
+
+| Status | Error             | Description                                                 |
+| ------ | ----------------- | ----------------------------------------------------------- |
+| 401    | Invalid Collector | collectorId or collectorAppKey is invalid or doesn't match. |
+| 403    | Invalid Batch     | the batch is invalid or cannot be written now.              |
+
+#### complete()
+
+*Syntax:*
+
+`Promise(void) complete()`
+
+*Description:*
+
+Try to complete the current batch. It may wait for a short time depends on situation of the batch, and may throw the following Http errors in case of failure.
+
+| Status | Error             | Description                                                 |
+| ------ | ----------------- | ----------------------------------------------------------- |
+| 401    | Invalid Collector | collectorId or collectorAppKey is invalid or doesn't match. |
+| 403    | Invalid Batch     | the batch is invalid or cannot be written now.              |
+
+#### cancel()
+
+*Syntax:*
+
+`Promise(void) cancel()`
+
+*Description:*
+
+Try to cancel the current batch. It may throw the following Http errors in case of failure.
+
+| Status | Error             | Description                                                 |
+| ------ | ----------------- | ----------------------------------------------------------- |
+| 401    | Invalid Collector | collectorId or collectorAppKey is invalid or doesn't match. |
+| 403    | Invalid Batch     | the batch is invalid or cannot be written now.              |
+
+### FileField
+
+### constructor()
+
+*Syntax:* 
+
+`constructor(String localPath)`
+
+*Description:*
+
+It create a FileField instance with the following arguments:
+
+- localPath: The local path of the file
+
+
+
---
+++ index.mjs
@@ -0,0 +1,162 @@
+import axios from 'axios';
+import fs from 'fs';
+import path from 'path';
+import FormData from 'form-data';
+import { createHash } from 'crypto';
+
+export class DataBatch {
+    constructor(server, collector, appKey, name) {
+        this.server = server;
+        this.collector = collector;
+        this.appKey = appKey;
+        this.name = name;
+        this.queue = [];
+        this.interval;
+    }
+
+    async init() {
+        let response = await this.postServer('/api/storage/versions', { name: this.name });
+        this.version = response.data.version;
+        this.timeout = response.data.timeout * 1000; // response timeout in s
+        this.stop();
+        this.interval = setInterval(() => this.heartbeat(), this.timeout / 5);
+        this.lat = Date.now();
+    }
+
+    async heartbeat() {
+        if (Date.now() - this.lat >= 2 * this.timeout / 5) {
+            await this.postServer(`/api/storage/versions/${this.version}/heartbeat`);
+        }
+    }
+
+    stop() {
+        if (this.interval) {
+            clearInterval(this.interval);
+            this.interval = null;
+        }
+    }
+
+    async add(type, json) {
+        this.queue.push({ type, json });
+
+        if (this.queue.length >= (this.options?.buffer || 100)) {
+            await this.flush();
+        }
+    }
+
+    async flush() {
+        if (this.queue.length > 0) {
+            let instances = [];
+            let files = [];
+
+            // handle files one by one, and accumulate instances
+            for (let i of this.queue) {
+                let inst = await handleFiles(i.json);
+                instances.push({ type: i.type, body: inst.body, files: inst.files.map(i => i.key) });
+                inst.files.forEach(i => files.push(i));
+            }
+
+            // prepare files at first
+            let findResponse = await this.postServer(`/api/file/find`, files.map(i => i.key));
+            let fileMap = new Set();
+            findResponse.data.forEach(i=>fileMap.add(i));
+            let uploadFiles = files.filter(i => !fileMap.has(i.key));
+            for (let i of uploadFiles) {
+                var formData = new FormData();
+                formData.append('file', fs.createReadStream(i.file), i.key);
+                let response = await this.postServer('/api/file/files', formData);
+
+                // We shall assert equaity of our own name and server's result
+                if(response.data.fileName != i.key) {
+                    throw new Error('Inconsist File Upload');
+                }
+            }
+
+            // then add the instance
+            let response = await this.postServer(`/api/storage/versions/${this.version}/instances`, instances);
+
+            // reset queue at last
+            this.queue = [];
+        }
+    }
+
+    async complete() {
+        await this.flush();
+        await this.postServer(`/api/storage/versions/${this.version}/complete`);
+        this.stop();
+    }
+
+    async cancel() {
+        await this.postServer(`/api/storage/versions/${this.version}/cancel`);
+        this.stop();
+    }
+
+    postServer(url, data) {
+        let result = axios.post(this.server + url, data || {}, { headers: { collector: this.collector, appkey: this.appKey } });
+        this.lat = Date.now(); // update lat when any request success
+        return result;
+    }
+}
+
+export class FileField {
+    constructor(path) {
+        this.path = path;
+    }
+}
+
+function asyncReadFile(path) {
+    return new Promise((resolve, reject) => fs.readFile(path, null, (err, data) => {
+        if (err) {
+            reject(err);
+        }
+        else {
+            resolve(data);
+        }
+    }));
+}
+
+// return { file, key }
+async function getFile(fileField) {
+    let fileName = fileField.path;
+    let data = await asyncReadFile(fileName);
+    let ext = path.extname(fileName);
+    let hex = createHash('sha256').update(data).update(ext).digest('hex');
+    return { file: fileName, key: hex + ext };
+}
+
+async function handleFiles(json) {
+    let files = [];
+    async function traverseObject(obj) {
+        switch (typeof (obj)) {
+            case 'object':
+                if (obj instanceof Array) {
+                    let ret = [];
+                    for (let i of obj) {
+                        ret.push(await traverseObject(i));
+                    }
+                    return ret;
+                }
+                else {
+                    let ret = {};
+                    for (let i in obj) {
+                        let val = obj[i];
+                        if (val instanceof FileField) {
+                            let file = await getFile(val);
+                            files.push(file);
+                            val = file.key;
+                        }
+                        else {
+                            val = await traverseObject(val);
+                        }
+                        ret[i] = val;
+                    }
+                    return ret;
+                }
+            default:
+                return obj;
+        }
+    }
+
+    let body = await traverseObject(json);
+    return { body, files };
+}
